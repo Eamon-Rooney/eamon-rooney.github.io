9a0ad254fc588dff594b161a4d987853b62139c1
Eamon Rooney <eamon.rooney@outlook.com>
2023-01-07 21:19:55 +0000

    change gameweek players based on dropdown

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FplService } from '../fpl/fpl.service';
 import { TeamService } from './team.service';
+import { EventList } from 'app/interfaces/events';
 
 @Component({
   selector: 'app-team',
@@ -10,16 +11,17 @@ import { TeamService } from './team.service';
 })
 export class TeamComponent implements OnInit {
 
+
   constructor(
     private _teamService: TeamService,
     private router: Router,
-    private _fplService: FplService) {
-    }
+    private _fplService: FplService) {}
 
   bootstrap: any;
   team: any;
   events: any;
-  gameweek: number = 19;
+  gameweek!: EventList;
+  gameweekID!: number;
 
   async ngOnInit() {
 
@@ -29,14 +31,23 @@ export class TeamComponent implements OnInit {
       console.log("BOOTSTRAP", response);
       this.events = this.bootstrap.events;
       console.log("EVENTS", this.events);
+
+      this.gameweek = this.events.filter((a: { [x: string]: boolean; }) => a['is_current'] === true);
+      console.log("CURRENTGAMEWEEK", this.gameweek);
+      this.gameweekID = this.gameweek[0].id;
+      console.log("CURRENTGAMEWEEKID", this.gameweekID);
+      sessionStorage.setItem('GameWeekID', JSON.stringify(this.gameweekID));
     });
 
     let entry = this.router.routerState.snapshot.root.children[0].url[1].path;
 
-    (await this._teamService.getTeamPicks(+entry, this.gameweek))
+    const sessionGameWeekID = sessionStorage.getItem('GameWeekID');
+    this.gameweekID = sessionGameWeekID !== null ? JSON.parse(sessionGameWeekID) : '';
+
+    (await this._teamService.getTeamPicks(+entry, this.gameweekID))
     .subscribe(response => {
       this.team = response;
-      console.log("TEAMPICKS", response);
+      console.log("TEAMPICKS", this.team);
     });
 
     this.router.routeReuseStrategy.shouldReuseRoute = () => {
@@ -44,20 +55,14 @@ export class TeamComponent implements OnInit {
     }
   }
 
-  // async updateGameweek() {
+  async updateGameweek() {
 
-  //   (await this._fplService.getFplBootstrap())
-  //   .subscribe(response => {
-  //     this.bootstrap = response;
-  //     console.log("BOOTSTRAP", response);
-  //   });
+    let entry = this.router.routerState.snapshot.root.children[0].url[1].path;
 
-  //   let entry = this.router.routerState.snapshot.root.children[0].url[1].path;
-
-  //   (await this._teamService.getTeamPicks(+entry, this.events[18].id))
-  //   .subscribe(response => {
-  //     this.events = response;
-  //     console.log("UPDATEGAMEWEEK", response);
-  //   });
-  // }
+    (await this._teamService.getTeamPicks(+entry, this.gameweekID))
+    .subscribe(response => {
+      this.team = response;
+      console.log("UPDATEGAMEWEEK", this.team);
+    });
+  }
 }
