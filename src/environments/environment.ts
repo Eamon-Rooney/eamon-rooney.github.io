b7d61d9c42c8d3544d2cbf20772edcc12a651df5
Eamon Rooney <eamon.rooney@outlook.com>
2024-07-27 21:50:59 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
 
   fpl: {
     bootstrap: (corsanywhere + 'bootstrap-static/'),
-    league: (corsanywhere + 'leagues-classic/517177/standings/'),
+    league: (corsanywhere + 'leagues-classic/175725/standings/'),
     fixtures: (corsanywhere + 'fixtures/?event='),
   },
   team: {
